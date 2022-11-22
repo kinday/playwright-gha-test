909e7ec72b827caf40f526372f20c2fbe2293e71
Leonard Kinday <leonard@kinday.se>
2022-11-22 16:32:55 +0100

    feat: add base app

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("#test-form")
+
+const message = document.createElement("div")
+
+function validate({ username, password }) {
+  if (username.length < 1) {
+    return "Username is required"
+  } else if (password.length < 1) {
+    return "Password is required"
+  } else if (username !== "kinday" && password !== "Abc123") {
+    return "Incorrect credentials"
+  }
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  if (message.parentNode) {
+    message.parentNode.removeChild(message)
+  }
+
+  const formData = new FormData(event.target)
+  const username = formData.get("username")
+  const password = formData.get("password")
+
+  const errors = validate({ username, password })
+
+  if (errors == null) {
+    message.innerText = "Welcome kinday!"
+    form.replaceWith(message)
+  } else {
+    message.innerText = errors
+    form.prepend(message)
+  }
+})
---
+++ tests/example.spec.js
@@ -0,0 +1,65 @@
+// @ts-check
+const { test, expect } = require("@playwright/test")
+
+test("validates username presence", async ({ page }) => {
+  await page.goto("/")
+
+  await expect(page).toHaveTitle(/Parcel/)
+
+  const submitButton = page.getByRole("button", { name: "Submit" })
+  await submitButton.click()
+
+  const message = page.getByText("Username is required")
+  await expect(message).toBeVisible()
+})
+
+test("validates password presence", async ({ page }) => {
+  await page.goto("/")
+
+  await expect(page).toHaveTitle(/Parcel/)
+
+  const usernameInput = page.getByLabel("Username")
+  await usernameInput.type("kinday")
+
+  const submitButton = page.getByRole("button", { name: "Submit" })
+  await submitButton.click()
+
+  const message = page.getByText("Password is required")
+  await expect(message).toBeVisible()
+})
+
+test("validates credentials", async ({ page }) => {
+  await page.goto("/")
+
+  await expect(page).toHaveTitle(/Parcel/)
+
+  const usernameInput = page.getByLabel("Username")
+  await usernameInput.type("foobar")
+
+  const passwordInput = page.getByLabel("Password")
+  await passwordInput.type("bazqux")
+
+  const submitButton = page.getByRole("button", { name: "Submit" })
+  await submitButton.click()
+
+  const message = page.getByText("Incorrect credentials")
+  await expect(message).toBeVisible()
+})
+
+test("logs in", async ({ page }) => {
+  await page.goto("/")
+
+  await expect(page).toHaveTitle(/Parcel/)
+
+  const usernameInput = page.getByLabel("Username")
+  await usernameInput.type("kinday")
+
+  const passwordInput = page.getByLabel("Password")
+  await passwordInput.type("Abc123")
+
+  const submitButton = page.getByRole("button", { name: "Submit" })
+  await submitButton.click()
+
+  const message = page.getByText("Welcome kinday!")
+  await expect(message).toBeVisible()
+})
