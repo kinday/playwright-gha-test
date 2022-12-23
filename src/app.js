bda916b82a5aa3255973ee930e87151f586bce50
Leonard Kinday <leonard@kinday.se>
2022-12-23 11:59:52 +0100

    feat: add Monaco editor

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import * as monaco from "monaco-editor"
+
 const form = document.querySelector("#test-form")
 
 const message = document.createElement("div")
@@ -36,4 +38,53 @@ form.addEventListener("submit", (event) => {
     message.innerText = errors
     form.prepend(message)
   }
+})
+
+self.MonacoEnvironment = {
+  getWorker: function (workerId, label) {
+    const getWorkerModule = (moduleUrl, label) => {
+      return new Worker(self.MonacoEnvironment.getWorkerUrl(moduleUrl), {
+        name: label,
+        type: "module",
+      })
+    }
+
+    switch (label) {
+      case "json":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/json/json.worker?worker",
+          label
+        )
+      case "css":
+      case "scss":
+      case "less":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/css/css.worker?worker",
+          label
+        )
+      case "html":
+      case "handlebars":
+      case "razor":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/html/html.worker?worker",
+          label
+        )
+      case "typescript":
+      case "javascript":
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/language/typescript/ts.worker?worker",
+          label
+        )
+      default:
+        return getWorkerModule(
+          "/monaco-editor/esm/vs/editor/editor.worker?worker",
+          label
+        )
+    }
+  },
+}
+
+monaco.editor.create(document.getElementById("monaco-container"), {
+  value: "function hello() {\n\talert('Hello world!');\n}",
+  language: "javascript",
 })
